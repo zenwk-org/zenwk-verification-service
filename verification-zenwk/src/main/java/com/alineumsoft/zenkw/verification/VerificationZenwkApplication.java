df90006bce5bb9577506d628b82f6a069555b170
calegria <calegria@MacBook-Pro-de-calegria.local>
2025-03-10 18:31:12 -0500

    V-10/03/2025 - Estrucura incial del proyecto
    Estrucura incial del proyecto

---
+++ verification-zenwk/src/main/java/com/alineumsoft/zenkw/verification/VerificationZenwkApplication.java
@@ -0,0 +1,13 @@
+package com.alineumsoft.zenkw.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationZenwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificationZenwkApplication.class, args);
+	}
+
+}
